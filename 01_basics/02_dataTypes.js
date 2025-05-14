2e4be0c57492b3985207c7c35c512878f1ad0160
kamal81684 <127001908+kamal81684@users.noreply.github.com>
2025-05-14 17:27:18 +0000

    configure file done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict" // treat all js code to newer version
+
+// alert(3 + 3)  //will give error bcz we r using nodejs not browser or browser it will treat evrthing as text
+
+/*
+    data types
+    1. number
+    2. boolean
+    3. string
+    4. null = standlone value
+    5. undefined
+    6. symbol = for uniqness
+
+
+    * object
+
+
+
+*/
+
+console.log(typeof null);  //object
